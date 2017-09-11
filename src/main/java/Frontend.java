e72d30f13df805e8426d7dc4bc838d3652a53050
darik17 <darik17@yandex.ru>
2017-09-12 01:46:40 +0300

    Настройка maven. Решение в сервлете

---
+++ src/main/java/Frontend.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+
+    private String login = " ";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println(req.getParameter("key"));
+    }
+
+
+}
